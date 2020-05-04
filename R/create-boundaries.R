2c4562b68668f3678f24337cb418ccb53d212a03
ben_bellman <benjamin_bellman@brown.edu>
2020-05-04 08:59:18 -0400

    Adding maps of trends in past week

--- R/create-boundaries.R
+++ R/create-boundaries.R
@@ -5,8 +5,17 @@ library(dplyr)
 
 # load county boundaries
 county_bnd <- counties(cb = T) %>% 
-  st_as_sf() %>% 
-  select(STATEFP, NAME)
+  st_as_sf()
+
+# paste " City" at the end of six independent cities to distinguish them from counties
+county_bnd[county_bnd$STATEFP == 24 & county_bnd$COUNTYFP == 510, "NAME"] <- "Baltimore City"
+county_bnd[county_bnd$STATEFP == 29 & county_bnd$COUNTYFP == 510, "NAME"] <- "St. Louis City"
+county_bnd[county_bnd$STATEFP == 51 & county_bnd$COUNTYFP == 600, "NAME"] <- "Fairfax City"
+county_bnd[county_bnd$STATEFP == 51 & county_bnd$COUNTYFP == 620, "NAME"] <- "Franklin City"
+county_bnd[county_bnd$STATEFP == 51 & county_bnd$COUNTYFP == 760, "NAME"] <- "Richmond City"
+county_bnd[county_bnd$STATEFP == 51 & county_bnd$COUNTYFP == 770, "NAME"] <- "Roanoke City"
+
+county_bnd <- select(county_bnd, STATEFP, NAME)
 
 # combine NYC, re-add to counties
 nyc <- filter(county_bnd, STATEFP == 36 & NAME %in% c("New York", "Kings", "Queens", "Bronx", "Richmond"))
@@ -39,8 +48,23 @@ county_bnd <- county_bnd %>%
   st_transform(2163) %>% 
   st_simplify(dTolerance = 1000) #1km
 
+
 # save boundaries
-st_write(county_bnd, "~/Documents/Computer Backup/Repos/modeling-covid/data/nyt_boundaries.shp", delete_layer = T)
+st_write(county_bnd, "~/Documents/Computer Backup/Repos/mapping-covid/data/nyt_boundaries.shp", delete_layer = T)
+
+
+# get states and save as file to remove download step
+states <- states(cb = T) %>% 
+  st_as_sf() %>% 
+  st_transform(2163) %>% 
+  st_simplify(dTolerance = 2000) %>% #2km
+  mutate(STATEFP = as.numeric(STATEFP)) %>% 
+  filter(STATEFP <= 56) %>% 
+  filter(STATEFP %in% c(2, 15) == F)
+
+st_write(states, "~/Documents/Computer Backup/Repos/mapping-covid/data/state_boundaries.shp", delete_layer = T)
+
+
 
 
 ### Twitter tips
--- R/most-recent-maps.R
+++ R/most-recent-maps.R
@@ -5,17 +5,23 @@ library(sf)
 library(ggplot2)
 library(rcartocolor)
 library(ghibli)
-library(tigris)
 library(lubridate)
 library(here)
 library(data.table)
 library(stringr)
+library(broom)
 
+#### data preparation ####
 
 # load most update NYT file
 nyt <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv") %>% 
-  select(-fips) %>% 
-  mutate(county = str_remove(county, " city"))
+  mutate(
+    county = str_remove(county, " city"),
+    county = if_else(fips %in% c(29510, 24510, 51620, 51600, 51770, 51760),
+                     paste0(county, " City"),
+                     county)
+  ) %>% 
+  select(-fips)
 
 # keep only most recent counts
 today <- filter(nyt, date == max(date))
@@ -23,6 +29,10 @@ today <- filter(nyt, date == max(date))
 # load prepared boundary polygons
 bnd <- st_read(here::here("data", "nyt_boundaries.shp"), stringsAsFactors = F) %>% 
   filter(state %in% c("Alaska", "Hawaii") == F)
+
+# load prepared state boundaries
+states <- st_read(here("data", "state_boundaries.shp"))
+
 
 # add today's counts to boundary file, format and drop AK and HI
 covid <- left_join(bnd, today) %>% 
@@ -52,15 +62,7 @@ covid <- left_join(bnd, today) %>%
     )
   )
 
-# get state boundaries
-states <- states(cb = T) %>% 
-  st_as_sf() %>% 
-  st_transform(2163) %>% 
-  st_simplify(dTolerance = 2000) %>% #2km
-  mutate(STATEFP = as.numeric(STATEFP)) %>% 
-  filter(STATEFP <= 56) %>% 
-  filter(STATEFP %in% c(2, 15) == F)
-
+#### raw count maps ####
 
 
 # do a simple map of confirmed cases
@@ -100,7 +102,7 @@ ggplot(covid) +
 
 
 ########## 
-# Per capita maps
+#### Per capita maps ####
 
 # load population data
 pop <- read_csv(here::here("data", "county_populations_nyt.csv"))
@@ -187,6 +189,137 @@ ggplot(covid_pop) +
   ggsave(here::here("maps", paste0("deaths-per-capita_", unique(today$date), ".png")), height = 4, width = 6.44)
 
 
+#### temporal maps ####
+
+# write a custom function that adds necessary variables to county-slice of data
+add_trend_vars <- function(county){
+  if(nrow(county) < 8) NA else {
+    county %>% 
+      # calculate daily new cases and deaths, and add numeric "time" variable
+      mutate(
+        new_cases = cases - c(0, embed(cases, dimension = 2)[,2]),
+        new_deaths = deaths - c(0, embed(deaths, dimension = 2)[,2]),
+        time = 0:7
+      ) %>% 
+      # drop additional reference day from data
+      filter(date > min(date))
+  }
+}
 
 
 
+# get last week of covid data, plus extra day for comparison
+last_week <- nyt %>% 
+  filter(date > max(date) - 8) %>% 
+  # run calculations within each county
+  group_by(state, county) %>% 
+  group_split() %>% 
+  map(add_trend_vars) %>% 
+  .[is.na(.) == F] %>% 
+  bind_rows() %>% 
+  # replace any negative values (due to reporting changes I think?) with 0
+  mutate(
+    new_cases = if_else(new_cases < 0, 0, new_cases),
+    new_deaths = if_else(new_deaths < 0, 0, new_deaths)
+  )
+
+
+# get slope for time regression on new cases
+last_week %>% 
+  group_by(state, county) %>% 
+  group_split() %>% 
+  map_dbl(
+    as_mapper(
+      ~ lm(new_cases ~ time, data = .x)$coefficients[2]
+    )
+  ) -> case_trend
+
+# get slope for time regression on new deaths
+last_week %>% 
+  group_by(state, county) %>% 
+  group_split() %>% 
+  map_dbl(
+    as_mapper(
+      ~ lm(new_deaths ~ time, data = .x)$coefficients[2]
+    )
+  ) -> death_trend
+
+# create spatial file to map trends
+last_week %>% 
+  select(state, county) %>% 
+  unique() %>% 
+  cbind(case_trend) %>% 
+  cbind(death_trend) %>% 
+  as_tibble() %>% 
+  # join to county boundaries
+  right_join(bnd) %>% 
+  # create mapping categories
+  mutate(
+    case_trend_cat = case_when(
+      case_trend <= -10 ~ "10+ fewer",
+      case_trend > -10 & case_trend <= -1 ~ "1-10 fewer",
+      case_trend > -1 & case_trend < 1 ~ "No change",
+      case_trend >= 1 & case_trend < 10 ~ "1-10 more",
+      case_trend >= 10 ~ "10+ more",
+      TRUE ~ "No cases"
+    ),
+    death_trend_cat = case_when(
+      death_trend <= -1 ~ "1+ fewer",
+      death_trend > -1 & death_trend <= -0.5 ~ "0.5-1 fewer",
+      death_trend > -0.5 & death_trend < 0.5 ~ "No change",
+      death_trend >= 0.5 & death_trend < 1 ~ "0.5-1 more",
+      death_trend >= 1 ~ "1+ more",
+      TRUE ~ "No cases"
+    ),
+    case_trend_cat = factor(case_trend_cat, levels = c("No cases",
+                                                       "10+ fewer",
+                                                       "1-10 fewer",
+                                                       "No change",
+                                                       "1-10 more",
+                                                       "10+ more")),
+    death_trend_cat = factor(death_trend_cat, levels = c("No cases",
+                                                         "1+ fewer",
+                                                         "0.5-1 fewer",
+                                                         "No change",
+                                                         "0.5-1 more",
+                                                         "1+ more"))
+  ) -> week_trends
+
+
+# map of case trend in past week by county
+ggplot() +
+  geom_sf(data = week_trends, aes(geometry = geometry, fill = case_trend_cat), size = 0) +
+  geom_sf(data = states, fill = NA, lwd = 0.2) +
+  scale_fill_manual(values = c("white", carto_pal(5, "Geyser"))) +
+  labs(title = "Confimed COVID-19 Cases in Past Week", 
+       subtitle = paste0(max(nyt$date) - 7, " to ", max(nyt$date)), 
+       fill = "Daily change\nin cases", 
+       caption = "Data from The New York Times\nMap by @BenInquiring") +
+  theme_bw() +
+  theme(
+    panel.grid = element_blank(),
+    axis.ticks = element_blank(),
+    axis.text = element_blank(),
+    panel.border = element_blank()
+  ) +
+  ggsave(here::here("maps", paste0("cases-trend_", unique(today$date), ".png")), height = 4, width = 6.44)
+
+
+
+# map of death trend in past week by county
+ggplot() +
+  geom_sf(data = week_trends, aes(geometry = geometry, fill = death_trend_cat), size = 0) +
+  geom_sf(data = states, fill = NA, lwd = 0.2) +
+  scale_fill_manual(values = c("white", carto_pal(5, "Geyser"))) +
+  labs(title = "Confimed COVID-19 Cases in Past Week", 
+       subtitle = paste0(max(nyt$date) - 7, " to ", max(nyt$date)), 
+       fill = "Daily change\nin death", 
+       caption = "Data from The New York Times\nMap by @BenInquiring") +
+  theme_bw() +
+  theme(
+    panel.grid = element_blank(),
+    axis.ticks = element_blank(),
+    axis.text = element_blank(),
+    panel.border = element_blank()
+  ) +
+  ggsave(here::here("maps", paste0("deaths-trend_", unique(today$date), ".png")), height = 4, width = 6.44)
